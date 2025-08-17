e1d00d03e460ccacf3e33053e274c3a3f0cd55e5
Dinar Jahan <110754416+DinarPerseus@users.noreply.github.com>
2025-08-17 23:33:05 +0600

    Completed module1

---
+++ Assignment of Module 1/Task2.cs
@@ -0,0 +1,15 @@
+﻿int[] storeIntegerData = new int[5];
+
+Console.WriteLine("Please enter 5 integer values:");
+for (int i = 0; i < storeIntegerData.Length; i++)
+{
+    storeIntegerData[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Data stored successfully.");
+
+Console.Write("Stored data: ");
+for (int i = 0; i < storeIntegerData.Length; i++)
+{
+    Console.Write(storeIntegerData[i]+" ");
+}
